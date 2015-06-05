da6f626aa53adc547deb9c12de0ac1a4c56d18f2
Vignesh Shanmugam <vignesh.shanmugam@flipkart.com>
2015-06-05 18:39:30 +0530

    Fix

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ window.addEventListener('DOMContentLoaded', function() {
     if (startstop.data('status') === 'running')
       chartContext.series[0].addPoint(o.z);
 
-  	if(o.z > 0 && o.z < 1){
+  	if(o.z > 0 && o.z < 3){
   		captureImage();
   	}
 
