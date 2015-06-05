33d5e0ec75f746c82565ab123ee1e12d7d658e2e
Boopathi Rajaa <me@boopathi.in>
2015-06-05 22:34:05 +0530

    Removes unnecessary stuff

--- js/main.js
+++ js/main.js
@@ -9,8 +9,6 @@ var PRINTF = function(o, msg, val) {
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
-gyro.frequency = 10;
-
 function success(stream){
   videoAvailable = true;
   if (video.mozSrcObject !== undefined) {
@@ -33,15 +31,11 @@ function error(err){
 	console.log('Error' + error);
 }
 
-
 $(function() {
   var meter = document.getElementById('meter');
-  var zdiv = document.getElementById('zs');
-
-  var max = { x: 0, y: 0, z: 0 };
-  var min = { x: Infinity, y: Infinity, z: Infinity };
-  // var zs = [];
   var chartContext;
+
+  gyro.frequency = 1;
 
   var startstop = $('#startstop');
   var count = 1;
@@ -49,7 +43,6 @@ $(function() {
   video = document.getElementById('video');
   canvas = document.getElementById('imgCanvas');
   ctx = canvas.getContext('2d');
-
 
   setTimeout(function() {
     navigator.getUserMedia({video: true}, success, error);
@@ -66,53 +59,15 @@ $(function() {
     }
   });
 
-  $('#timeline').highcharts({
-    chart: {
-      zoomType: 'x',
-      events: { load: function() { chartContext = this; } }
-    },
-    series: [{
-      type: 'area',
-      data: [0]
-    }]
-  });
-
   gyro.startTracking(function(o) {
-
-    // find max
-    max.x = MAX(max.x, o.x);
-    max.y = MAX(max.y, o.y);
-    max.z = MAX(max.z, o.z);
-
-    min.x = MIN(min.x, o.x);
-    min.y = MIN(min.y, o.y);
-    min.z = MIN(min.z, o.z);
-
-    // zs.push(o.z);
-    if (startstop.data('status') === 'running')
-      chartContext.series[0].addPoint(o.z);
 
   	if(Math.abs(o.z) > 0 && Math.abs(o.z) < 1.5){
   		   captureImage();
   	}
 
-    meter.innerHTML = "";
-    // PRINTF(meter, 'alpha', o.alpha);
-    // PRINTF(meter, 'beta', o.beta);
-    // PRINTF(meter, 'gamma', o.gamma);
-
-    // PRINTF(meter, 'max x', max.x);
-    // PRINTF(meter, 'max y', max.y);
-    // PRINTF(meter, 'max z', max.z);
-
-    // PRINTF(meter, 'min x', min.x);
-    // PRINTF(meter, 'min y', min.y);
-    // PRINTF(meter, 'min z', min.z);
     PRINTF(meter,'z',Math.abs(o.z));
     PRINTF(meter,'y',Math.abs(o.y));
     PRINTF(meter,'x',Math.abs(o.x));
-
-
 
   });
 
