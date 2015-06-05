08e1f7d2b26bda224ebdf65319892d43f9344825
Boopathi Rajaa <me@boopathi.in>
2015-06-05 15:34:30 +0530

    fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var MAX = function(a,b) { return a>b?a:b; };
+var MIN = function(a,b) { return a<b?a:b; };
 
 var PRINTF = function(o, msg, val) { o.innerHTML += msg + ' = ' + val.toString() + '<br/>'; };
 
@@ -8,6 +9,7 @@ window.addEventListener('DOMContentLoaded', function() {
   var fallingDown = false;
 
   var max = { x: 0, y: 0, z: 0 };
+  var min = { x: 0, y: 0, z: 0 };
 
   gyro.startTracking(function(o) {
 
@@ -21,6 +23,11 @@ window.addEventListener('DOMContentLoaded', function() {
     max.y = MAX(max.y, o.y);
     max.z = MAX(max.z, o.z);
 
+    min.x = MIN(min.x, o.x);
+    min.y = MIN(min.y, o.y);
+    min.z = MIN(min.z, o.z);
+
+
     meter.innerHTML = "";
     PRINTF(meter, 'alpha', o.alpha);
     PRINTF(meter, 'beta', o.beta);
@@ -29,6 +36,10 @@ window.addEventListener('DOMContentLoaded', function() {
     PRINTF(meter, 'max x', max.x);
     PRINTF(meter, 'max y', max.y);
     PRINTF(meter, 'max z', max.z);
+
+    PRINTF(meter, 'min x', min.x);
+    PRINTF(meter, 'min y', min.y);
+    PRINTF(meter, 'min z', min.z);
 
   });
 });
