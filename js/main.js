cbdcd78c9ab99b3ed33bdddaf18784b587714395
Vignesh Shanmugam <vignesh.shanmugam@flipkart.com>
2015-06-05 15:25:01 +0530

    Checking

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 window.addEventListener('DOMContentLoaded', function() {
   var meter = document.getElementById('meter');
 
+  var fallingDown = false;
+
   gyro.startTracking(function(o) {
+
+
+  	if(o.alpha && o.alpha > 180){
+  		fallingDown = true;
+  		console.log(o.alpha);
+  	}
+
     meter.innerHTML = "";
     meter.innerHTML += 'alpha = ' + o.alpha + '<br/>';
     meter.innerHTML += 'beta = ' + o.beta + '<br/>';
